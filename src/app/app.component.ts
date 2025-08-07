6987e76daaf0fa15341f2a0b3e282d1131ef8b02
aman-doceree <mohd.aman@doceree.com>
2025-08-07 14:46:21 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'task_1';
+
+Contactform: FormGroup;
+
+  constructor() {
+    this.Contactform = new FormGroup({
+      firstname: new FormControl('', Validators.required),
+      lastname: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      gender: new FormControl('', Validators.required), 
+      age: new FormControl('', [Validators.required, Validators.min(0)]),
+      DOB: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required)
+    
+    });
+  }
+
+
+  onSubmit(){
+    if (this.Contactform.valid) {
+      console.log('Form Submitted!', this.Contactform.value);
+      // Here you can handle the form submission, e.g., send data to a server
+    } else {
+      console.log('Form is invalid');
+    }
+
+  }
+ 
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,20 @@ import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ManageComponent } from './manage/manage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
